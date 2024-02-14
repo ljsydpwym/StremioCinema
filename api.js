7ec58739fbb896b0ddc09e73429ba3c858cc37c2
Patrik Kábele <pkabele@retia.cz>
2024-02-14 11:30:21 +0100

    FIX: Lepší nastavovat headery na 1 miste, nez zbytecne u kazde funkce

--- api.js
+++ api.js
@@ -2,12 +2,15 @@ const Logger = require('./logger.js')
 const needle = require('needle');
 
 const logger = new Logger("CALL", false)
-
-async function call(method, url, data, options) {
-    logger.log("Api request", arguments)
-    const response = await needle(method, url, data, options)
-    logger.log("Api response", response.body)
-    return response
+const headers = {
+	"user-agent": "Kodi/16.1 (Patched; Intel Something) App_Bitness/64 Version/17.6-Git:20171114-a9a7a20",
+	"X-UuuId": "a9301413-b900-470d-9aaf-813758c66214"
+};
+async function call(method, url, data, options = {headers: headers} ) {
+	logger.log("Api request", arguments)
+	const response = await needle(method, url, data, options)
+	logger.log("Api response", response.body)
+	return response
 }
 
 module.exports = call
--- sc.js
+++ sc.js
@@ -11,8 +11,8 @@ const token = "access_token=9ajdu4xyn1ig8nxsodr3"
 
 class SC {
 	PREFIX = "scc:"
-    constructor() {
-    }
+	constructor() {
+	}
 
 	getWithPrefix(id){
 		return `${this.PREFIX}${id}`
@@ -22,56 +22,41 @@ class SC {
 		return id.replace(this.PREFIX, "")
 	}
 
-    uuidv4() {
-      return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-      );
-    }
-
-	getHeaders(){
-	    return {
-             headers: {
-                 "User-Agent": "XBMC/13.0-ALPHA3 Git:20130430-e8fe5cf (Windows NT 6.1;WOW64;Win64;x64; http://www.xbmc.org)",
-                 "X-Uuid": this.uuidv4(),
-             }
-       }
-	}
-
 	async search(value,type="*") {
-		var ret = (await call(`get`, `${baseUrl}/filter/v2/search?type=${type}&order=desc&sort=score&value=${encodeURIComponent(value)}&${token}`, undefined, this.getHeaders())).body
+		var ret = (await call(`get`, `${baseUrl}/filter/v2/search?type=${type}&order=desc&sort=score&value=${encodeURIComponent(value)}&${token}`)).body
 		ret = JSON.parse(ret)
 		logger.log("search", arguments, ret)
 		return ret
 	}
 
 	async searchFrom(type,from=0){
-		let ret = (await call(`get`,`${baseUrl}/filter/v2/news?type=${type}&sort=dateAdded&order=desc&days=365&${token}&from=${from}`, undefined, this.getHeaders())).body
+		let ret = (await call(`get`,`${baseUrl}/filter/v2/news?type=${type}&sort=dateAdded&order=desc&days=365&${token}&from=${from}`)).body
 		ret = JSON.parse(ret)
 		logger.log("search", arguments, ret)
 		return ret
 	}
 
-    async service(id, type) {
-        var ret = (await call(`get`, `${baseUrl}/filter/v2/service?type=${type}&service=tmdb&value=${id}&${token}`, undefined, this.getHeaders())).body
-        ret = JSON.parse(ret)
-        logger.log("service", arguments, ret)
-        return ret
-    }
+	async service(id, type) {
+		var ret = (await call(`get`, `${baseUrl}/filter/v2/service?type=${type}&service=tmdb&value=${id}&${token}`)).body
+		ret = JSON.parse(ret)
+		logger.log("service", arguments, ret)
+		return ret
+	}
 
-    async streams(id) {
-        var ret = (await call(`get`, `${baseUrl}/${id}/streams?${token}`, undefined, this.getHeaders())).body
-        logger.log("streams", arguments, ret)
-        return ret
-    }
+	async streams(id) {
+		var ret = (await call(`get`, `${baseUrl}/${id}/streams?${token}`)).body
+		logger.log("streams", arguments, ret)
+		return ret
+	}
 
 	async episode(id, season, episode ) {
-        const seasonsResponse = await this.parent(id)
-        const seasonId = (seasonsResponse.hits.hits.find(it => it._source.info_labels.season == season) ?? seasonsResponse.hits.hits[season-1])._id;
-        const episodesResponse = await this.parent(seasonId)
-        var ret = episodesResponse.hits.hits[episode-1]._id
-        logger.log("episode", arguments, ret)
-        return ret
-    }
+		const seasonsResponse = await this.parent(id)
+		const seasonId = (seasonsResponse.hits.hits.find(it => it._source.info_labels.season == season) ?? seasonsResponse.hits.hits[season-1])._id;
+		const episodesResponse = await this.parent(seasonId)
+		var ret = episodesResponse.hits.hits[episode-1]._id
+		logger.log("episode", arguments, ret)
+		return ret
+	}
 
 	async episodes(id){
 		const seasonsResponse = await this.parent(id);
@@ -86,17 +71,17 @@ class SC {
 	}
 
 	async media(id){
-		const ret = (await call(`get`, `${baseUrl}/${id}?${token}`, undefined, this.getHeaders())).body
+		const ret = (await call(`get`, `${baseUrl}/${id}?${token}`)).body
 		logger.log("media", arguments, ret)
 		return ret
 	}
 
-    async parent(id) {
-        var ret = (await call(`get`, `${baseUrl}/filter/v2/parent?value=${id}&sort=episode&${token}`, undefined, this.getHeaders())).body
-        ret = JSON.parse(ret)
-        logger.log("parent", arguments, ret)
-        return ret
-    }
+	async parent(id) {
+		var ret = (await call(`get`, `${baseUrl}/filter/v2/parent?value=${id}&sort=episode&${token}`)).body
+		ret = JSON.parse(ret)
+		logger.log("parent", arguments, ret)
+		return ret
+	}
 
 }
 
