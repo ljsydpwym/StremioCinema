29ecfdecb706fc35e8ff49e1fe548fab58820076
Patrik Kábele <Airsane@gmail.com>
2024-02-16 00:23:39 +0100

    Revert "FIX: Lepší nastavovat headery na 1 miste, nez zbytecne u kazde funkce"
    This reverts commit 7ec58739fbb896b0ddc09e73429ba3c858cc37c2.

--- api.js
+++ api.js
@@ -2,15 +2,12 @@ const Logger = require('./logger.js')
 const needle = require('needle');
 
 const logger = new Logger("CALL", false)
-const headers = {
-	"user-agent": "Kodi/16.1 (Patched; Intel Something) App_Bitness/64 Version/17.6-Git:20171114-a9a7a20",
-	"X-UuuId": "a9301413-b900-470d-9aaf-813758c66214"
-};
-async function call(method, url, data, options = {headers: headers} ) {
-	logger.log("Api request", arguments)
-	const response = await needle(method, url, data, options)
-	logger.log("Api response", response.body)
-	return response
+
+async function call(method, url, data, options) {
+    logger.log("Api request", arguments)
+    const response = await needle(method, url, data, options)
+    logger.log("Api response", response.body)
+    return response
 }
 
 module.exports = call
